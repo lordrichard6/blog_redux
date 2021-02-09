e9510e9a88fc7d47b3f0195c59cc03bc97fb9994
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-09 18:50:09 +0100

    switch statements in reducers

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,16 @@
-export default () => {
-  return 123;
+export default (state = [], action) => {
+  //   if (action.type === "FETCH_POSTS") {
+  //     return action.payload;
+  //   }
+
+  //   return state;
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload;
+    // case "abc":
+    //   return action.etc;
+    // ...
+    default:
+      return state;
+  }
 };
