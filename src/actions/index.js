57f73ad76dead3245aebee89136825933b9ec8d2
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-09 17:44:12 +0100

    redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,15 @@
 import jsonplaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
+// export const fetchPosts = () => {
+//   return async function (dispatch) {
+//     const response = await jsonplaceholder.get("/posts");
+
+//     dispatch({ type: "FETCH_POSTS", payload: response });
+//   };
+// };
+
+export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get("/posts");
 
-  return {
-    type: "FETCH_POST",
-    payload: response,
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
