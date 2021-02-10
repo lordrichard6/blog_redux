531c4060c8541d9e18bca7db89c43b355c1ff7d9
lordrichard6 <paulolopesreizinho@gmail.com>
2021-02-10 13:01:48 +0100

    alternate overfetching solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,7 @@ import jsonplaceholder from "../apis/jsonPlaceholder";
 //   };
 // };
 
+//More clean function
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get("/posts");
 
@@ -18,10 +19,18 @@ export const fetchPosts = () => async (dispatch) => {
 // export const fetchUser = (id) => (dispatch) => {
 //   _fetchUser(id, dispatch);
 // };
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+//More clean function
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
+
+export const fetchUser = id => async dispatch => {
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
